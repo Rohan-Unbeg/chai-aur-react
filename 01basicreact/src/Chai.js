abc430e338faafa8c1ed382c57916d61de36c302
Rohan Unbeg <rohanunbeg0918@gmail.com>
2025-03-11 21:54:05 +0530

    finished lecture 1,2

---
+++ 01basicreact/src/Chai.js
@@ -0,0 +1,7 @@
+function Chai(){
+    return (
+        <h1>Chai from function</h1>
+    )
+}
+
+export default Chai
---
+++ 01vitereact/src/Chai.jsx
@@ -0,0 +1,5 @@
+function Chai(){
+    return <h1>hi from chai function</h1>
+}
+
+export default Chai
