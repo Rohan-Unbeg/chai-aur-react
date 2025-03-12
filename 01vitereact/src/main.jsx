4ee7d21ee41a08c6155b93dc9f3a5726ba69d8ce
Rohan Unbeg <rohanunbeg0918@gmail.com>
2025-03-12 21:49:03 +0530

    done with custom react

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,9 +1,45 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+function MyApp() {
+    return <h1>hello there</h1>;
+}
+
+// const reactElement = {
+//     type: "a",
+//     props: {
+//         href: "https://www.google.com",
+//         target: "_blank",
+//     },
+//     children: "go to google.com",
+// };
+
+const anotherElement = <a href="https://google.com">google.com</a>;
+
+const reactElement = React.createElement(
+    "a",
+    { href: "www.google.com", target: "_blank" },
+    "click me to visit google",
+    anotherElement
+);
+
+const username = "rohan";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <>
+  {reactElement}
+  <h1>Hello, {username}</h1>
+  </>
+);
+
+
+// ReactDom.createRoot(document.getElementById("root")).render(
+//     <>
+//         {/* <App /> */}
+//         {/* <MyApp /> */}
+//         <MyApp />
+//         {/* MyApp() */}
+//         {reactElement}
+//     </>
+// );
